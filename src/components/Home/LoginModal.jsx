943f8085538279a86b794aff0c2354da2511f098
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-05-25 14:48:02 -0700

    test

--- src/components/Home/LoginModal.jsx
+++ src/components/Home/LoginModal.jsx
@@ -6,17 +6,17 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   background-color: blue;
-  height: 55vh;
-  width: 35vw;
+  height: 40vh;
+  width: 25vw;
 
   > form {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background-color: red;
+    /* background-color: red; */
     > input {
-      width: 50%;
+      width: 70%;
     }
   }
 `
@@ -29,6 +29,7 @@ const LoginModal = ( props ) => {
       <form>
         <input placeholder="username" />
         <input placeholder="password" />
+        <button>Enter</button>
       </form>
     </Container>
   )
