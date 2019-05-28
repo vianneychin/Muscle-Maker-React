c5a290cde4e7f36a2fb42c01cde35c0ccd145333
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-05-28 09:06:22 -0700

    delete service worker file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
-import LoginModal from './LoginModal'
+import React         from 'react'
+import styled        from 'styled-components'
+import LoginModal    from './LoginModal'
 import RegisterModal from './RegisterModal'
 
 //---------------styled-components-----------------
