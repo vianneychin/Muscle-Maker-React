3231b75366c283b60b3d10846429d1698206e422
Arrick <Grapes01@gmail.com>
2019-05-28 13:58:42 -0700

    Arrick: resigter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import                  './global.css';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import * as routes from './routes/routes'
 import React       from 'react';
 import Home        from './components/Home/Home'
@@ -7,6 +7,23 @@ import Dashboard   from './components/Dashboard/Dashboard'
 import Workouts    from './components/Workouts/Workouts'
 
 class App extends React.Component {
+  componentDidMount(){
+    this.getWorkouts()
+  }
+  handleRegister = async (data) => {
+    try {
+      const response = await fetch('http://localhost:8000/api/v1/workouts', {
+        credentials: 'include'
+      })
+
+      const responseParsed = await response.json()
+      console.log(responseParsed, 'workouts')
+      
+    } catch (err) {
+      console.log(err)
+      
+    }
+  }
   render() {
     return (
       <div>
