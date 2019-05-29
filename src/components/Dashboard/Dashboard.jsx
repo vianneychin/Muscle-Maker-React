7a9e8a635060f20f9fb71ffcab600bb46049d4cc
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-05-29 11:57:57 -0700

    Clean Code: Dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,20 +1,17 @@
-import React    from 'react'
-import { Link } from 'react-router-dom'
-import Clock from 'react-live-clock'
-import styled   from 'styled-components'
+import React            from 'react'
+import { Link }         from 'react-router-dom'
+import Clock            from 'react-live-clock'
+import styled           from 'styled-components'
 import YoutubeComponent from './YoutubeComponent/YoutubeComponent'
 
 
 /* <----------- styled components -----------> */
-const Wrapper = styled.div`
-`
 const HeaderBackground = styled.div`
   background-image: url('https://i.imgur.com/keKDODv.jpg?1');
   background-size: cover;
   background-position: top;
   height: 26em;
 `
-
 const Main = styled.main`
   display: flex;
   align-items: center;
@@ -28,18 +25,24 @@ const Main = styled.main`
 class Dashboard extends React.Component {
   render() {
     return (
-      <Wrapper>
+      <div>
         <HeaderBackground>
           <Main>
             <h2>Hey John</h2>
+
+            {/* <Clock/> is from 'react-live-clock' npm */}
             <h1>Today is Monday, <Clock format={'HH:mm:ssa'} ticking={true} /> </h1>
-            <Link to="/workouts"><h1>My Workouts ➞</h1></Link>
+
+            <Link to="/workouts">
+              <h1>My Workouts ➞</h1>
+            </Link>
           </Main>
         </HeaderBackground>
 
+        {/* COMPONENT */}
         <YoutubeComponent />
 
-      </Wrapper>
+      </div>
     )
   }
 }
