eaf895fff4e595383cba70434a99ac48b4fe9dad
Arrick <Grapes01@gmail.com>
2019-05-30 15:51:36 -0700

    logout

--- src/components/Workouts/MappedExercise.jsx
+++ src/components/Workouts/MappedExercise.jsx
@@ -4,14 +4,14 @@ class MappedExercise extends Component {
     render(){
         return this.props.workout.map((exercise, i) => {
             return(
-                <li key={i}> 
-                    <p>{exercise.muscle}</p>
-                    <p>{exercise.workout_name}</p>
-                    <p>{exercise.equipment}</p>
-                    <p>{exercise.weight}</p>
-                    <p>{exercise.sets}</p>
-                    <p>{exercise.reps}</p>
-                </li>
+                <ul key={i}> 
+                    <li>{exercise.muscle}</li>
+                    <li>{exercise.workout_name}</li>
+                    <li>{exercise.equipment}</li>
+                    <li>{exercise.weight}</li>
+                    <li>{exercise.sets}</li>
+                    <li>{exercise.reps}</li>
+                </ul>
         )
     })
 }
